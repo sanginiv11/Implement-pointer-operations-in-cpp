7744af15d45b911ba59e7fb9ff4cac07a1582f09
Sangini Verma <sanginiv11@gmail.com>
2025-08-28 14:37:42 +0530

    Add files via upload

---
+++ exp103.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+void Increment(int &s)
+{
+    s=s+5000;
+    cout<<"Salary inside function: "<<s<<endl;
+}
+int main()
+{
+    int sal=27000;
+    Increment(sal);
+    cout<<"Salary inside main: "<<sal<<endl;
+    return 0;
+}
